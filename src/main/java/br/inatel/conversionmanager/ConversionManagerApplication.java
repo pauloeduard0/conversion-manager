e6d79261d2b98fab3e6107aaab920fd02a22b742
Paulo Eduardo <68037168+pauloeduard0@users.noreply.github.com>
2023-06-22 13:38:10 -0300

    feat: spring boot startup files

---
+++ src/main/java/br/inatel/conversionmanager/ConversionManagerApplication.java
@@ -0,0 +1,13 @@
+package br.inatel.conversionmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversionManagerApplication.class, args);
+	}
+
+}
