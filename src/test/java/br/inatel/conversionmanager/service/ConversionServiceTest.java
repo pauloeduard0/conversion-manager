8ac0b33cb7ee584a46aff68279a16ff530c2146d
Paulo Eduardo <68037168+pauloeduard0@users.noreply.github.com>
2023-07-11 15:43:54 -0300

    feat: creating the unit test for the service

---
+++ src/test/java/br/inatel/conversionmanager/service/ConversionServiceTest.java
@@ -0,0 +1,140 @@
+package br.inatel.conversionmanager.service;
+
+import br.inatel.conversionmanager.adapter.ConversionAdapter;
+import br.inatel.conversionmanager.exception.CurrencyNotFoundException;
+import br.inatel.conversionmanager.model.dto.ConversionDto;
+import br.inatel.conversionmanager.model.dto.ExchangeRateResponse;
+import br.inatel.conversionmanager.model.entities.Conversion;
+import br.inatel.conversionmanager.repository.ConversionRepository;
+import br.inatel.conversionmanager.service.validation.DefaultValidator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(MockitoJUnitRunner.class)
+class ConversionServiceTest {
+
+    @Mock
+    List<DefaultValidator> defaultValidatorList;
+    @Mock
+    private ConversionRepository conversionRepository;
+    @Mock
+    private ConversionAdapter conversionAdapter;
+
+    @InjectMocks
+    private ConversionService conversionService;
+
+    private ConversionDto createConversionDto(Float amount, String to, LocalDate date, Float converted) {
+        return ConversionDto.builder()
+                .amount(amount)
+                .to(to)
+                .convertedAmount(converted)
+                .date(date)
+                .build();
+    }
+
+    private Conversion createConversion(Float amount, String tocurrency, LocalDate date, Float converted) {
+        return Conversion.builder()
+                .amount(amount)
+                .base("EURO")
+                .tocurrency(tocurrency)
+                .date(date)
+                .converted(converted)
+                .build();
+
+    }
+
+    @Test
+    void givenValidRequest_whenSaving_thenReturnSavedConversion() {
+        ExchangeRateResponse exchangeRateResponse = new ExchangeRateResponse(
+                1687996799L,
+                "EUR",
+                true,
+                Map.of(
+                        "ANG", 1.968256f,
+                        "SVC", 9.555569f,
+                        "CAD", 1.44658f,
+                        "XCD", 2.949964f,
+                        "USD", 1.091548f
+                ),
+                "2023-06-28",
+                true
+        );
+        when(conversionAdapter.getExchangeRates()).thenReturn(Collections.singletonList(exchangeRateResponse));
+
+        ConversionDto conversionDto = createConversionDto(500F, "USD", LocalDate.now(), null);
+
+        Conversion conversionToSave = createConversion(500F, "USD", LocalDate.now(), 545.774f);
+        Conversion savedConversion = createConversion(500F, "USD", LocalDate.now(), 545.774f);
+
+        when(conversionRepository.save(any(Conversion.class))).thenReturn(savedConversion);
+
+        ConversionDto result = conversionService.saveConversion(conversionDto);
+
+        assertEquals(savedConversion.getBase(), result.baseCurrency());
+        assertEquals(savedConversion.getAmount(), result.amount());
+        assertEquals(savedConversion.getTocurrency(), result.to());
+        assertEquals(savedConversion.getConverted(), result.convertedAmount());
+        assertEquals(savedConversion.getDate(), result.date());
+
+        verify(conversionAdapter).getExchangeRates();
+    }
+
+    @Test
+    void givenInvalidCurrency_whenSaving_thenThrowCurrencyNotFoundException() {
+        when(conversionAdapter.getExchangeRates()).thenReturn(Collections.emptyList());
+
+        ConversionDto conversionDto = createConversionDto(500F, "INVALID", LocalDate.now(), 0F);
+
+        assertThrows(CurrencyNotFoundException.class, () -> conversionService.saveConversion(conversionDto));
+
+        verify(conversionRepository, never()).save(any());
+    }
+
+    @Test
+    void testGetAllConversions() {
+        List<Conversion> conversionList = new ArrayList<>();
+        conversionList.add(createConversion(500F, "USD", LocalDate.now(), 600F));
+        conversionList.add(createConversion(800F, "GBP", LocalDate.now(), 900F));
+
+        when(conversionRepository.findAll(any(Pageable.class))).thenReturn(new PageImpl<>(conversionList));
+
+        Page<ConversionDto> result = conversionService.getAllConversions(Pageable.unpaged());
+
+        verify(conversionRepository).findAll(any(Pageable.class));
+
+        assertEquals(conversionList.size(), result.getContent().size());
+
+        List<ConversionDto> dtoList = result.getContent();
+        for (int i = 0; i < conversionList.size(); i++) {
+            Conversion conversion = conversionList.get(i);
+            ConversionDto dto = dtoList.get(i);
+
+            assertEquals(conversion.getBase(), dto.baseCurrency());
+            assertEquals(conversion.getAmount(), dto.amount());
+            assertEquals(conversion.getTocurrency(), dto.to());
+            assertEquals(conversion.getConverted(), dto.convertedAmount());
+            assertEquals(conversion.getDate(), dto.date());
+        }
+    }
+
+}
