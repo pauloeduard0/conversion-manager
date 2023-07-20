d314b3a4dbb736ad86d1030c0dba9beb158bd2e4
Paulo Eduardo <68037168+pauloeduard0@users.noreply.github.com>
2023-07-20 16:27:16 -0300

    refactor: remove unusable import

--- src/test/java/br/inatel/conversionmanager/service/ConversionServiceTest.java
+++ src/test/java/br/inatel/conversionmanager/service/ConversionServiceTest.java
@@ -26,7 +26,6 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
